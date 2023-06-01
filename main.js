f290b2e8a57948c659153af5ab429f9234c8a247
Melissa Colmenarez <melissa.colmenarez@signaturit.com>
2023-06-01 16:59:34 +0200

    version 1  curso javascript2

---
+++ main.js
@@ -0,0 +1,19 @@
+/*let nombre = 'Carlos';
+let numeros = 45; 
+let boleano = true;
+console.log(nombre, numeros, boleano);
+*/
+
+let numero1 = 10;
+let numero2 = 20;
+let numero3 = 30;
+let numero4 = 40;
+let suma = numero1 + numero2;
+let resta = numero2 - numero3;
+let multiplicacion = numero1 * numero4;
+let division = numero1 / numero3;
+
+console.log('resultado: ', suma);
+console.log('resultado: ', resta);
+console.log('resultado: ', multiplicacion);
+console.log('resultado: ', division, 'resultado: ', division);
