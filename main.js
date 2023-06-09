b23e1ff0513b66056252113bda1181acb54a1e1f
Melissa Colmenarez <melissa.colmenarez@signaturit.com>
2023-06-09 17:48:39 +0200

    avance

--- main.js
+++ main.js
@@ -19,8 +19,155 @@ console.log('resultado: ', multiplicacion);
 console.log('resultado: ', division, 'resultado: ', division);
 */
 
-let base = 50;
+/*let base = 50;
 let altura = 25;
 let ecuacion = (base + altura) / 2;
 
 console.log('El resultado de la ecuación: ', ecuacion);
+let pulgada = 1;
+let centimetros = 2.54;
+let totalPulgadas = 10; 
+let ecuacionPulgadas = (totalPulgadas * centimetros)/pulgada;
+
+console.log('La conversión de 10 pulgadas es igual: ' , ecuacionPulgadas);
+*/
+
+/*
+let numero1 = 100;
+let numero2 = 25;
+
+if (numero1 > numero2) {
+    console.log('numero1: ', numero1);
+    console.log('numero2: ', numero2);
+    console.log('numero1 es mayor');
+} else {
+    console.log('numero1: ', numero1);
+    console.log('numero2: ', numero2);
+    console.log('numero2 es mayor');
+}
+*/
+
+/*EJERCICIOS
+let edadAna = 12;
+let edadMaria = 15;
+
+if (edadAna < edadMaria) {
+    console.log('edadAna: ', edadAna);
+    console.log('edadMaria: ', edadMaria);
+    console.log('Ana es menor que María porque tiene: ', edadAna);
+} else {
+    (edadMaria > edadAna)
+    console.log('edadMaria: ', edadMaria);
+    console.log('edadAna: ', edadAna);
+    console.log('Maria es mayor que Ana porque tiene: ', edadMaria);
+}
+*/
+
+/*
+let precioTomates = 5;
+let precioPatatas = 10;
+
+if (precioPatatas > precioTomates) {
+    console.log('precioTomates: ', precioTomates);
+    console.log('precioPatatas: ', precioPatatas);
+    console.log('El precio de las patatas es mayor porque cuesta: ', precioPatatas);
+} else {
+    console.log('precioTomates: ', precioTomates);
+    console.log('precioPatatas: ', precioPatatas);
+    console.log('El precio de los tomates es menor porque cuesta: ', precioTomates);
+}
+*/
+/*
+CONDICIONES 
+Igual a 50 = Repetir
+Menor a 50 = Toma clases extra
+mayor a 50 = Suficiente
+Mayor a 80 = Bien 
+Mayor a 90 = Muy Bien
+Igual a 100 = Excelente
+
+*/
+
+
+/* EJEMPLO ELSE IF (IF dentro de un IF)
+let numero1 = 50;
+let numero2 = 50;
+let numero3 = 50;
+let numero4 = 50;
+let totalSuma = (numero1 + numero2 + numero3 + numero4) / 4;
+console.log('resultado: ', totalSuma);
+
+if (totalSuma == 100) {
+    console.log('Excelente');
+} else if (totalSuma > 90) {
+    console.log('Muy Bien');
+} else if (totalSuma > 80) {
+    console.log('Bien');
+} else if (totalSuma > 50) {
+    console.log('Suficiente')
+} else if (totalSuma < 50) {
+    console.log('Tomar clases extra')
+} else if (totalSuma == 50) {
+    console.log('Repetir')
+}
+*/
+
+/*MUESTRA EL MAYOR DE LOS 3 NUMEROS
+
+let numero1 = 5000;
+let numero2 = 7150;
+let numero3 = 3000;
+
+if (numero1 > numero2) {
+    if (numero1 > numero3) {
+        console.log(numero1);
+    } else {
+        console.log(numero3);
+    }
+} else {
+    if (numero2 > numero3) {
+        console.log(numero2);
+    } else {
+        console.log(numero3);
+    }
+}
+*/
+
+
+/*MUESTRA EL MENOR DE LOS 3 VALORES
+let numero1 = 100;
+let numero2 = 50;
+let numero3 = 10;ujikkkk
+
+if (numero1 < numero2) {
+    if (numero1 < numero3) {
+        console.log(numero1);
+    } else {
+        console.log(numero3)
+    }
+} else {
+    if (numero2 < numero3) {
+        console.log(numero2);
+    } else {
+        console.log(numero3);
+    }
+}
+*/
+
+
+/*OPERADORES DE COMPARACION
+== Igual que
+=== igual valor & tipo
+!= Diferente valor
+!== Diferente valor & Tipo
+>= Mayor igual
+<= Menor igual
+> Mayor
+> Menor
+? Ternario *******pendienteeee*********
+*/
+
+let nombre = window.prompt('Escribe tu nombre');
+console.log(nombre);
+
+
